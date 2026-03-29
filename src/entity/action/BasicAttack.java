791b0c7729a89e63b943597e0017bf605306f8cc
zhihong-czh <chanzhihong04@gmail.com>
2026-03-29 16:36:49 +0800

    update BasicAttack.java

--- src/entity/action/BasicAttack.java
+++ src/entity/action/BasicAttack.java
@@ -4,13 +4,18 @@ import entity.role.Combatant;
 
 public class BasicAttack implements Action {
     private final Combatant attacker;
+    private final Combatant target;
+    private final int attack;
 
     public BasicAttack(Combatant attacker, Combatant target, int attack) {
         this.attacker = attacker;
+        this.target = target;
+        this.attack = attack;
     }
 
     @Override
     public void execute(Combatant target) {
-        target.getAttack(attacker.getAttack());
+        Combatant victim = target != null ? target : this.target;
+        victim.getAttack(attacker.getAttack());
     }
 }
